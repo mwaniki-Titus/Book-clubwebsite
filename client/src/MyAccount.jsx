5466644cde79614100e5d05cdc666f5b44f8d55e
abdhata <abdhata@outlook.com>
2023-11-06 12:36:46 +0300

    Made cosmetic changes to the MyAccount component

--- client/src/MyAccount.jsx
+++ client/src/MyAccount.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Assuming you're using Material-UI Icons
 
 function MyAccount() {
   return (
     <div>
-      <h1>My Account</h1>
+      <h1>
+        <AccountCircleIcon style={{ marginRight: '10px' }} /> My Account
+      </h1>
       <p>View and manage your account details here.</p>
     </div>
   );
